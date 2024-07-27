6bc51a50f3c1f647b2db6a5d6e5aef879b716436
shyam <shyamsankar3100@gmail.com>
2024-07-27 19:16:00 +0530

    Org data

--- JAVA_25_TASK/Print_element_in_Array.java
+++
@@ -1,9 +0,0 @@
-public class Print_element_in_Array {
-    public static void main(String[]args){
-        int[] arr =new int[]{1,2,3,4,5,6,7,8};
-        System.out.println("THE ARRAY ELEMENT'S :");
-        for(int i=0;i<arr.length;i++){
-            System.out.println("INDEX ["+i+"] : "+arr[i]);
-        }
-    }
-}
